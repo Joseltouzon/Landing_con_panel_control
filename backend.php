d406a8467016ed22571e03e0022519f485639a4d
Jose Luis Lopez Touzon <josel.touzon@gmail.com>
2022-06-30 15:22:56 -0300

    Vista backend: backend.php y header del panel adaptados

--- backend.php
+++ backend.php
@@ -1,121 +1,128 @@
 <?php
 
 include_once('inc/dbconfig.php');
+include_once('inc/class.crud.compania.php');
+$compania = new COMPANIA();
+$companiaDetails = $compania->find($link, 1);
+$img = "images/compania/" . $companiaDetails['id'] . "/" . $companiaDetails['logo'];
+$img2 = "images/compania/" . $companiaDetails['id'] . "/" . $companiaDetails['favicon'];
 
-
-error_reporting(E_ALL);
-
-  if($_POST['usuario']=="admin" && $_POST['password']=='Lechuzablanca'){
-    $_SESSION['admin']='admin';
+if ($_POST['login'] != "") {
+  if ($_POST['usuario'] == "admin" && $_POST['password'] == 'Lechuzablanca') {
+    $_SESSION['admin'] = 'admin';
+  } else {
+    $mensaje = '<div class="alert alert-danger">Email y/o contrase&ntilde;a incorrectos</div>';
   }
-  if($_GET['close']=="logout"){
-    session_destroy();
-    // redirecciona despues de cerrar sesión
-    header("Location: backend.php");
-  }
-  
-  
+}
+if ($_GET['close'] == "logout") {
+  session_destroy();
+  // redirecciona despues de cerrar sesión
+  header("Location: backend.php");
+}
+
+
 ?>
 
 <!DOCTYPE html>
-<html>
-  <?php //include_once('includes/head.php');?>
-<?php /*  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> */ ?>
+<html lang="en" class="h-100">
+
 <head>
-  <meta charset="UTF-8">
-  <meta charset="ISO-8859-1">
-    
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <script src="js/jquery-1.11.2.min.js"></script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Log in Meme y Muma</title>
+  <!-- Favicon icon -->
+  <link rel="icon" type="image" sizes="16x16" href="<?= $img2 ?>">
+  <!-- Custom Stylesheet -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <script src="assets/js/modernizr-3.6.0.min.js"></script>
   <script type="text/javascript" src='js/picnet.table.filter.min.js'></script>
-
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-  <!-- Optional theme -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="ckeditor/ckeditor.js"></script>
 </head>
-    
-    <body>
-    
+<? if ($_SESSION['admin'] == '') { ?>
 
+  <body class="h-100">
+    <div id="preloader">
+      <div class="loader">
+        <svg class="circular" viewBox="25 25 50 50">
+          <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+        </svg>
+      </div>
+    </div>
+    <div class="bg-success h-100">
+      <div class="container h-100">
+        <div class="row justify-content-center h-100">
+          <div class="col-xl-6">
+            <div class="form-input-content">
+              <div class="card">
+                <div class="card-body">
+                  <div class="logo text-center">
 
-    <?if($_SESSION['admin']==''){?>
-      <div class="container">
-        <div class="col-lg-4">
-          <div class="well">
-            <form method="post">
-              <h4>Administrador</h4>
-              <input type="text" name="usuario" class="form-control" placeholder='Usuario'><br>
-              <input type="password" name="password" class="form-control" placeholder='Clave'><br>
-              <button class="form-control btn btn-primary" type="submit">Entrar</button>
-            </form>    
+                    <img src="<?= $img ?>" alt="" style="max-height: 90px;">
+
+                  </div>
+                  <h4 class="text-center m-t-15">Ingresar al Panel de Control</h4>
+                  <form class="m-t-30 m-b-30" method="post">
+                    <?php echo $mensaje ?>
+                    <div class="form-group">
+                      <label>Usuario</label>
+                      <input type="text" class="form-control" placeholder="E-mail Usuario" name="usuario">
+                    </div>
+                    <div class="form-group">
+                      <label>Password</label>
+                      <input type="password" class="form-control" placeholder="Password" name="password">
+                    </div>
+                    <div class="text-center m-b-15 m-t-15">
+                      <input type="hidden" name="login" value="1">
+                      <button type="submit" class="btn btn-primary">Ingresar</button>
+                    </div>
+                  </form>
+                  <div class="text-center">
+                    <p>Desarrollado por <a href="http://www.sisifostudio.com" target="_blank">sisifostudio.com</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>  
-      
-    <?}else{?>
-      <?include_once('backend/includes/admin_header.php');?><br>
-      <div style="max-width:1400px;margin:0 auto;">
-      <?
-        if(!empty($_GET['a'])){  
-          include_once('backend/'.$_GET['a'].'.php'); 
-        }else{
-          include_once('backend/post.php');
-        }
-      ?>
-      </div>  
-    <?}?>
-            
-<?if($_REQUEST['e']!='ok'){?>
-    <!-- scripts de modificacion y eliminacion -->
-    <script>
-      $(document).ready(function(){
-        $('#table').tableFilter();
-        
-
-        $(".eliminaCat2").click(function(){
-          if(confirm("¿Desea eliminar?")){
-            var form = $(this).attr("rel");
-            $("#form2"+form).submit();
-          }
-        });
-        
-        $(".confirmaCat2").click(function(){
-          if(confirm("¿Desea modificar?")){
-            var form = $(this).attr("rel");
-            $("#formConfirma2"+form).submit();
-          }
-        });
-
-        $(".Eliminar").click(function(){
-          if(confirm("¿Desea Eliminar?")){
-            var form = $(this).attr("rel");
-            $("#formElimina"+form).submit();
-          }
-        });
-        
-        $(".editar").click(function(){
-          var form = $(this).attr("rel");
-          $("#formModifica"+form).submit();
-        });
-      });
-    </script>
-<?}?>
-<script type="text/javascript" >
-      // Replace the <textarea id="editor1"> with a CKEditor
-      // instance, using default configuration.
-      CKEDITOR.replace( 'editor' );
-      CKEDITOR.replace( 'editor2' );
-      CKEDITOR.replace( 'editor3' );
-  </script>
-   
+      </div>
+    </div>
+    <!-- #/ container -->
+    <!-- Common JS -->
+    <script src="assets/plugins/common/common.min.js"></script>
+    <!-- Custom script -->
+    <script src="assets/js/custom.min.js"></script>
   </body>
+
+<? } else { ?>
+
+  <body class="v-light vertical-nav fix-header fix-sidebar">
+    <div style="background:#F1F4F5 !important;">
+      <?php
+      include_once('backend/includes/header.php'); ?>
+      <br>
+
+      <?php
+      if (!empty($_GET['a'])) {
+        include_once('backend/' . $_GET['a'] . '.php');
+      } else {
+        include_once('backend/post.php');
+      }
+      ?>
+
+      <!-- Common JS -->
+      <script src="assets/plugins/common/common.min.js"></script>
+      <!-- Custom script -->
+      <script src="assets/js/custom.min.js"></script>
+      <script type="text/javascript">
+        // Replace the <textarea id="editor1"> with a CKEditor
+        // instance, using default configuration.
+        CKEDITOR.replace('editor');
+        CKEDITOR.replace('editor2');
+        CKEDITOR.replace('editor3');
+      </script>
+  </body>
+<? } ?>
+
 </html>
 
-<?php mysqli_close($link);?>
+<?php mysqli_close($link); ?>
